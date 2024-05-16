7d0b720729935d6294aa948a0f4e04db5f3d0ffb
Nico Ceresa <ceresanico@gmail.com>
2024-05-15 21:02:01 -0700

    added base functions to model.py and links to papers in papers_url.txt

---
+++ model.py
@@ -0,0 +1,35 @@
+"""
+RegNet Implimentation 
+
+by Nico Ceresa
+"""
+import torch
+from torch import nn
+
+class ConvLSTM(nn.Module):
+    """
+    Implimentation of the ConvLSTM
+
+    Args:
+        nn (_type_): _description_
+    """
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def forward(self, x):
+        pass
+
+
+class RegNet(nn.Module):
+    """
+    RegNet Class containing init and forward methods
+    """
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def forward(self, x):
+        pass
+    
+        
