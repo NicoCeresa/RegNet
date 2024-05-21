e4d8b1d78b19b32e8c1d077b1557409f366f6564
Nico Ceresa <ceresanico@gmail.com>
2024-05-21 16:05:11 -0700

    updated the convLSTMCell class

--- model.py
+++ model.py
@@ -4,20 +4,56 @@ RegNet Implimentation
 by Nico Ceresa
 """
 import torch
+import numpy as np
 from torch import nn
+from typing import List
 
-class ConvLSTM(nn.Module):
+class ConvLSTMCell(nn.Module):
     """
     Implimentation of the ConvLSTM
 
     Args:
         nn (_type_): _description_
     """
-    def __init__(self):
+    def __init__(self, in_dim, hidden_dim, kernel_size, padding, bias):
         super().__init__()
-        pass
+        self.in_dim = in_dim
+        self.hidden_dim = hidden_dim
+        self.padding = padding
+        self.bias = bias
+        self.conv = nn.Conv2d(in_channels=self.in_dim,
+                              out_channels=self.hidden_dim,
+                              kernel_size=self.kernel_size,
+                              padding=self.padding,
+                              bias = self.bias)
+
+        self.W_c = None
+        self.W_f = None
+        self.W_o = None
+        
+    def sigmoid(self, x):
+        return 1/(1 + np.exp(-x))
     
-    def forward(self, x):
+    def tanh(self, x):
+        num = np.exp(x) - np.exp(-x)
+        denom = np.exp(x) + np.exp(-x)
+        return num/denom
+    
+    def forward(self, x, h, c):
+        
+        it = self.sigmoid(self.conv(x) + self.conv(h) + c * self.W_c)
+        ft = self.sigmoid(self.conv(x) + self.conv(h) + c * self.W_f)
+        
+        Ct = np.multiply(ft, c) + np.multiply(it, self.conv(x) + self.conv(h))
+        ot = self.sigmoid(self.conv(x) + self.conv(h) + Ct * self.W_o)
+        
+        Ht = np.multiply(ot, self.tanh(Ct))
+        
+        return Ht, Ct
+    
+        
+class ConvLSTM(nn.Module):
+    def __init__(self):
         pass
 
 
