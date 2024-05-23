2b9231d018a5c3a127d4cb8b94f512a318c82fd5
Nico Ceresa <ceresanico@gmail.com>
2024-05-23 15:19:27 -0700

    updated RegNet class

--- scripts/ConvLSTM.py
+++ scripts/ConvLSTM.py
@@ -7,6 +7,7 @@ import torch
 import numpy as np
 from torch import nn
 from typing import List
+from torch.auto_grad import Variable
 
 device = 'cuda' if torch.cuda.is_available else 'cpu'
 
@@ -45,10 +46,10 @@ class ConvLSTMCell(nn.Module):
         denom = np.exp(x) + np.exp(-x)
         return num/denom
     
-    def init_hidden_weights(self, batch_size, img_size):
+    def init_hidden(self, batch_size, img_size):
         height, width = img_size.shape
-        return (torch.zeros(batch_size, self.hidden_dim, height, width, device=device),
-                torch.zeros(batch_size, self.hidden_dim, height, width, device=device))
+        return (Variable(torch.zeros(batch_size, self.hidden_dim, height, width, device=device)),
+                Variable(torch.zeros(batch_size, self.hidden_dim, height, width, device=device)))
     
     def forward(self, x, h, c):
         
@@ -73,40 +74,47 @@ class ConvLSTM(nn.Module):
         kernel_size: List
         steps=2: int
     """
-    def __init__(self, input_channels, hidden_channels, kernel_size, steps=2):
+    def __init__(self, input_channels, hidden_channels, kernel_size):
         self.input_channels = input_channels
         self.hidden_channels = hidden_channels
         self.kernel_size = kernel_size
         self.num_layers = len(hidden_channels)
-        self.steps = steps
         self.cell_layers = []
         for i in range(self.num_layers):
             cell = ConvLSTMCell(self.input_channels[i], self.hidden_channels[i], self.kernel_size[i])
             self.cell_layers.append(cell)
-            name = 'cell{}'.format(step)
-            setattr(self, name, cell)
+
             
     def forward(self, x, hidden_state=None):
+        """
+
+        Args:
+            x (_type_): 5D Tensor -> (seq_len, batches, channels, height, width)
+            hidden_state (_type_, optional): _description_. Defaults to None.
+        """
         # hold the states (H, C) so we can easily acces prev
         states = []
-        outputs = []
-        # shape (b, H, W)
-        batch_size, _, height, width = x.size()
-        for step in range(self.steps):
-            for i in range(self.num_layers):
-                name = 'cell{}'.format(i)
-                if step == 0:
-                    h, c = self._init_hidden_weights(batch_size=batch_size, img_size=(height, width))
-                    states.append((h,c))
-                    
-                (h, c) = states[i]
-                x, c_new = getattr(self, name)(x, h, c)
-                state[i] = (x, c_new)
-                outputs.append(x)
-        return outputs, (x, c_new)
+        # shape (seq_len, batches, channels, height, width) -> (batches, seq_len, channels, height, width)
+        curr_input = x.permute(1, 0, 2, 3, 4)
+        b, sequences, _, height, width = curr_input.shape
+        
+        if not hidden_state:
+            hidden_state = self._init_hidden(batch_size=b,
+                                             img_size=(height, width))
+        
+        for layer in range(self.num_layers):
+            h, c = hidden_state[layer]
+            output_inner = []
+            for i in range(sequences):
+                h_c = self.cell_layers[layer]
+                output_inner.append(h_c[0])
+                
+            states.append(h_c)
+            curr_input = torch.cat(output_inner, 0).view(curr_input.size(0), *outputs_inner[0].size()) # (seq_len, batches, channels, H, W)
+        return curr_input, states
         
         
-    def _init_hidden_weights(self, batch_size, img_size):
+    def _init_hidden(self, batch_size, img_size):
         height, width = img_size.shape
         init_hiddens = []
         for layer in range(self.num_layers):
--- scripts/RegNet.py
+++ scripts/RegNet.py
@@ -2,110 +2,93 @@
 RegNet Implimentation 
 
 by Nico Ceresa
+
+1) RNN-Regulated ResNet Module (RegNet module): The
+illustration of RegNet module is shown in Fig. 3(a). Here, we
+choose ConvLSTM for expounding. 𝐻
+𝑡−1 denotes the earlier
+output from ConvLSTM, and 𝐻𝑡
+is output of the ConvLSTM
+at 𝑡-th module . 𝑋𝑡_𝑖 denotes the 𝑖-th feature map at the 𝑡-th module.
+The 𝑡-th RegNet(ConvLSTM) module can be expressed as
+BN - Batch Normalization
+* - Convolution Operation
+
+1) Xt_2 = ReLU(BN(Wt_12 * Xt_1 + bt_12))
+
+2) [Ht, Ct] = ReLU(BN(ConvLSTM(Xt_2, [H{t-1}, C{t-1}])))
+-> need to grab [-1] index of outputs
+
+3) Xt_3 = ReLU(BN(Wt_23 * np.cat(Xt_2, Ht)))
+->Ht is the output from ConvLSTM
+
+4) Xt_4 = BN(Wt_34 * Xt_3 + bt_34)
+
+5) X{t+1}_1 = ReLU(Xt_1 + Xt_4)
+
+-> 3x3 Kernels: Wt_12 & Wt_34
+-> 1x1 Kernel: Wt_23
+
 """
 import torch
 import numpy as np
 from torch import nn
 from typing import List
+from ConvLSTM import ConvLSTM
 
 device = 'cuda' if torch.cuda.is_available else 'cpu'
 
-class ConvLSTMCell(nn.Module):
+class RegNetCell(nn.Module):
     """
-    Implimentation of a single ConvLSTM cell
-
-    Args:
-       in_dim: int 
-       hidden_dim: int
-       kernel_size: int 
-       padding: int
-       bias: int
+    RegNet Class containing init and forward methods
     """
-    def __init__(self, in_dim, hidden_dim, kernel_size, padding, bias):
+    def __init__(self, input_channels, hidden_channels, bias):
         super().__init__()
-        self.in_dim = in_dim
-        self.hidden_dim = hidden_dim
-        self.padding = padding
+        self.input_channels = input_channels
+        self.hidden_channels = hidden_channels
         self.bias = bias
-        self.conv = nn.Conv2d(in_channels=self.in_dim,
-                              out_channels=self.hidden_dim,
-                              kernel_size=self.kernel_size,
-                              padding=self.padding,
+        self.conv1 = self.conv = nn.Conv2d(in_channels=self.input_channels,
+                              out_channels=self.hidden_channels,
+                              kernel_size=1,
                               bias = self.bias)
-
-        self.W_c = None
-        self.W_f = None
-        self.W_o = None
-        
-    def sigmoid(self, x):
-        return 1/(1 + np.exp(-x))
-    
-    def tanh(self, x):
-        num = np.exp(x) - np.exp(-x)
-        denom = np.exp(x) + np.exp(-x)
-        return num/denom
+        self.conv3 = self.conv = nn.Conv2d(in_channels=self.input_channels,
+                              out_channels=self.hidden_channels,
+                              kernel_size=3,
+                              bias = self.bias)
+        self.ConvLSTM = ConvLSTM(kernel_size=3)
+        self.ReLU = nn.ReLU()
+        self.BN = nn.BatchNorm2d
     
     def init_hidden_weights(self, batch_size, img_size):
         height, width = img_size.shape
-        return (torch.zeros(batch_size, self.hidden_dim, height, width, device=device),
-                torch.zeros(batch_size, self.hidden_dim, height, width, device=device))
+        return (Variable(torch.zeros(batch_size, self.hidden_dim, height, width, device=device)),
+                Variable(torch.zeros(batch_size, self.hidden_dim, height, width, device=device)))
+        
+    def forward(self, x, H, C):
+        H, C = hidden_state[-1]
+        
+        Xt_2 = self.ReLU(self.BN(self.conv(x)))
+        
+        H, C = self.ReLU(self.BN(self.ConvLSTM(Xt_2, [H, C]))) 
+        
+        X_H = np.cat(Xt_2, H)
+        Xt_3 = self.ReLU(self.BN(self.conv1(X_H)))
+        Xt_4 = self.BN(self.conv3(Xt_3))
+        X_out = self.ReLU(x + Xt_4)
+        return X_out, H, C
     
-    def forward(self, x, h, c):
-        
-        it = self.sigmoid(self.conv(x) + self.conv(h) + c * self.W_c)
-        ft = self.sigmoid(self.conv(x) + self.conv(h) + c * self.W_f)
-        
-        Ct = np.multiply(ft, c) + np.multiply(it, self.conv(x) + self.conv(h))
-        ot = self.sigmoid(self.conv(x) + self.conv(h) + Ct * self.W_o)
-        
-        Ht = np.multiply(ot, self.tanh(Ct))
-        
-        return Ht, Ct
+class RegNet(nn.Module):
     
-        
-class ConvLSTM(nn.Module):
-    """
-    Full ConvLSTM with multiple layers consisting of the ConvLSTMCell class
-
-    Args:
-        input_channels: List
-        hidden_channels: List
-        kernel_size: List
-        steps=2: int
-    """
-    def __init__(self, input_channels, hidden_channels, kernel_size, steps=2):
+    def __init__(self, input_channels, hidden_channels, kernel_size, num_layers):
         self.input_channels = input_channels
         self.hidden_channels = hidden_channels
         self.kernel_size = kernel_size
-        self.num_layers = len(hidden_channels)
-        self.steps = steps
+        self.num_layers = num_layers
         self.cell_layers = []
         for i in range(self.num_layers):
-            cell = ConvLSTMCell(self.input_channels[i], self.hidden_channels[i], self.kernel_size[i])
+            cell = RegNetCell(self.input_channels[i], self.hidden_channels[i], self.kernel_size[i])
             self.cell_layers.append(cell)
-            name = 'cell{}'.format(step)
-            setattr(self, name, cell)
-            
-    def forward(self, x, hidden_state=None):
-        # hold the states (H, C) so we can easily acces prev
-        states = []
-        outputs = []
-        # shape (b, H, W)
-        batch_size, _, height, width = x.size()
-        for step in range(self.steps):
-            for i in range(self.num_layers):
-                name = 'cell{}'.format(i)
-                if step == 0:
-                    h, c = self._init_hidden_weights(batch_size=batch_size, img_size=(height, width))
-                    states.append((h,c))
-                    
-                (h, c) = states[i]
-                x, c_new = getattr(self, name)(x, h, c)
-                state[i] = (x, c_new)
-                outputs.append(x)
-        return outputs, (x, c_new)
-        
-        
+    
     def _init_hidden_weights(self, batch_size, img_size):
         height, width = img_size.shape
         init_hiddens = []
@@ -113,17 +96,17 @@ class ConvLSTM(nn.Module):
             init_hiddens.append(self.cell_layers[layer].init_hidden_weights(batch_size=batch_size, img_size=img_size))
         return init_hiddens
         
-
-
-class RegNet(nn.Module):
-    """
-    RegNet Class containing init and forward methods
-    """
-    def __init__(self):
-        super().__init__()
-        pass
-    
-    def forward(self, x):
-        pass
-    
+    def forward(self, x, hidden_state = None):
+        states = []
+        outputs = []
+        # x.size = B, C, H, W
+        batch_size, _, height, width = x.shape()
+        if not hidden_state:
+            hidden_state = self._init_hidden_weights(batch_size, (height, width))
+            
+        for layer in range(self.num_layers):
+            X, H, C = self.cell_layers[layer]
+            states.append((H, C))
+            outputs.append(X)
         
+        return outputs
